4759958ee26bd29ba6a2b75f5597fcd32663d960
sh-sahil <sahil.g.sharma7@gmail.com>
2024-04-30 22:06:28 +0530

    made the footer dynamic by derived states

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
       <Logo />
       <Form onAddItem={handelAddItems} />
       <PackingList items={items} onDeleteItem={handelDeleteItem} onToggleItem={handelToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -97,10 +97,21 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return <p className="stats">Start adding some items to your packing list 🚀</p>;
+  }
+
+  const itemLength = items.length;
+  const itemPacked = items.filter(item => item.packed).length;
+  const itemPercentage = Math.round((itemPacked / itemLength) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {itemPercentage === 100
+          ? "You have packed all the items... Ready to go✈️"
+          : `💼 You have ${itemLength} items on your list, and you already packed ${itemPacked} ${itemPercentage}%`}
+      </em>
     </footer>
   );
 }
